2d1c33be3aa8bfca7cb4bc295fcbebf53b9fd677
Hackbright Student <info@hackbrightacademy.com>
2014-03-18 14:55:48 -0700

    creating program to parse JSON for D3

--- static/js/json_to_D3_tree.js
+++ static/js/json_to_D3_tree.js
@@ -1,4 +1,6 @@
-var define =
+// takes in a JSON object and reformats object to comply with D3 tree format
+
+var json =
     {
          "define": [
               "define",
@@ -22,35 +24,82 @@ var define =
     };
 
 
+
+var createTree = function(json){
+  var tree = [];
+  // to start off, need to access the only key in the dictionary
+  for (var key in json){
+    new_node = {
+      "name": key,
+      "children": []
+    };
+    
+    expression_list = json[key];
+    children = expression_list.slice(1);
+    console.log(children);
+    for (var child in children){
+      new_node["children"].push(createNode(child));
+    }
+  }
+  return tree;
+};
+
+
+var createNode = function(args){
+  var parent = args[0];
+
+  new_node = {
+    "name": parent,
+    "children": []
+  };
+
+  if (parent=="define"){
+    var define = parent;
+    var variable = args[1];
+    var exp = args[2];
+
+    var define_var = createNode(variable);
+    var define_exp = createNode(exp);
+  
+    new_node["children"].push(define_var, define_exp);
+  }
+
+  // return final tree
+  return new_node;
+};
+
+
+tree = createTree(json);
+
+
 var treeData = [
   {
     "name": "define",
+    // "parent": "null",
     "children": [
       {
         "name": "area",
+      },
+      {
+        "name": "lambda",
         "children": [
           {
-            "name": "lambda",
+            "name": "r"
+          },
+          {
+            "name": "*",
             "children": [
               {
-                "name": "r"
+                "name": "3.141592653",
               },
               {
                 "name": "*",
                 "children": [
                   {
-                    "name": "3.141592653",
+                    "name": "r",
                   },
                   {
-                    "name": "*",
-                    "children": [
-                      {
-                        "name": "r",
-                      },
-                      {
-                        "name": "r"
-                      }
-                    ]
+                    "name": "r"
                   }
                 ]
               }
@@ -66,32 +115,31 @@ var treeData = [
 // var tree = [
 //   {
 //     "name": "define",
+//     // "parent": "null",
 //     "children": [
 //       {
 //         "name": "area",
+//       },
+//       {
+//         "name": "lambda",
 //         "children": [
 //           {
-//             "name": "lambda",
+//             "name": "r"
+//           },
+//           {
+//             "name": "*",
 //             "children": [
 //               {
-//                 "name": "r"
+//                 "name": "3.141592653",
 //               },
 //               {
 //                 "name": "*",
 //                 "children": [
 //                   {
-//                     "name": "3.141592653",
+//                     "name": "r",
 //                   },
 //                   {
-//                     "name": "*",
-//                     "children": [
-//                       {
-//                         "name": "r",
-//                       },
-//                       {
-//                         "name": "r"
-//                       }
-//                     ]
+//                     "name": "r"
 //                   }
 //                 ]
 //               }
@@ -103,4 +151,6 @@ var treeData = [
 //   }
 // ];
 
-console.log(JSON.stringify(treeData)==JSON.stringify(tree));
+
+// console.log(JSON.stringify(treeData)==JSON.stringify(tree));
+// oblivious to white space
