de9c6e7835dbf2c47a0ecdf044cae5e1ffcd94ae
Hackbright Student <info@hackbrightacademy.com>
2014-03-19 12:48:14 -0700

    database populated by two examples

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker, scoped_session
 # from sqlalchemy.orm import sessionmaker, scoped_session
 
 engine = create_engine("sqlite:///code.db", echo=False)
@@ -9,10 +10,20 @@ s = scoped_session(sessionmaker(
                                 autocommit = False,
                                 autoflush = False))
 Base = declarative_base()
+Base.query = s.query_property()
 
 ### Class declarations go here
 class Code(Base):
-    pass
+    __tablename__ = "programs"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=True)
+    description = Column(String(64), nullable=True)
+    code = Column(String(64))
+
+    # s.query(Code).filter_by(id=1).one()
+    # s.query(Code).filter_by(name='area').one()
+
+
 
 ### End class declarations
 
