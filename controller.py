321cc41ceae610295a5097d24698c75509ead9a7
Hackbright Student <info@hackbrightacademy.com>
2014-03-19 12:55:12 -0700

    modifying controller for database call

--- controller.py
+++ controller.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
 import lis
+import db
 
 app = Flask(__name__)
 app.secret_key = "thisisasecret"
 
-# asks user to input Scheme code
 @app.route("/")
 def index():
+    # asks user to input Scheme code
     html = render_template("index.html")
     return html
 
 @app.route("/get_json", methods=["POST"])
 def code_submitted():
+    # return JSON to ajax call -- code input by user
     user_input = request.form.get("user_input")
 
     if not user_input:
@@ -21,7 +23,14 @@ def code_submitted():
         json_object = lis.return_json(user_input)
         print json_object
 
-    # return JSON to ajax call
+    return json_object
+
+@app.route("/get_db_code/<code_name>")
+def get_db_code(code_name):
+    # return JSON to ajax call -- code from database
+    code_object = db.s.query(db.Code).filter_by(name=code_name).one()
+    code = code_object.code
+    json_object = lis.return_json(code)
     return json_object
 
 if __name__ == "__main__":
