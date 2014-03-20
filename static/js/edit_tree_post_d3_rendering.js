d9f6b8ff69f42abf87ac2a8e22028b4ea264fe5b
Hackbright Student <info@hackbrightacademy.com>
2014-03-20 15:22:23 -0700

    custom render tree

--- static/js/edit_tree_post_d3_rendering.js
+++ static/js/edit_tree_post_d3_rendering.js
@@ -1,25 +1,32 @@
 // color procedure nodes
 
 function editTree(){
-    nodes = document.getElementsByClassName("node");
-    circle = document.getElementsByTagName("circle");
-    text = document.getElementsByTagName("text");
-    // console.log(nodes);
-    // for (var i in nodes){
-    //     console.log(nodes[i]);
-    //     // console.log(circle);
-    // }
+    var nodes = $('.node');
+    var i;
+    // circle = document.getElementsByTagName("circle");
+    // text = document.getElementsByTagName("text");
+    console.log(nodes);
+    for (i=0; i<nodes.length; i++){
+        node = nodes[i];
+        // console.log(node);
+        circle = $(node).children('circle')[0];
+        text = $(node).children('text')[0];
 
-    for (var i = 0; i < text.length; i++){
-        node_label = (text[i].innerHTML);
-        console.log(node_label);
-        if (node_label == 'lambda'){
-            text[i].innerHTML = "<b>lambda</b>";
-            console.log('FIXED IT', text[i]);
+        if (text.innerHTML == '*'){
+            $(circle).css('stroke', '#fff');
         }
-        // console.log(text[i]);
-        // console.log(text[i]);
+        console.log(text.innerHTML);
     }
-}
 
+    // for (var i = 0; i < text.length; i++){
+    //     node_label = (text[i].innerHTML);
+    //     console.log(node_label);
+    //     if (node_label == 'lambda'){
+    //         text[i].innerHTML = "<b>lambda</b>";
+    //         console.log('FIXED IT', text[i]);
+    //     }
+        // console.log(text[i]);
+        // console.log(text[i]);
+//     }
+}
 
